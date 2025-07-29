4327c750c5fd443fe9d682e83c7493c676243205
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-07-30 04:24:02 +0530

    cards are now dealt

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -32,12 +32,22 @@ impl Deck {
          let mut rng = thread_rng();
          self.cards.shuffle(&mut rng);
     }
+
+    fn deal(&mut self, num_cards: usize) -> Vec<String>{
+        self.cards.split_off(
+            self.cards.len() - num_cards
+        )
+    }
 }
 
 fn main() {
     let mut deck = Deck::new();
     deck.shuffle();
-    println!("this is your deck of cards: {:#?}", deck);
+    let cards = deck.deal(3);
+    //need to add error handeling.
+
+    println!("this is your hand: {:#?}", cards);
+    println!("this is your deck: {:#?}", deck);
     // we use this {:#?} to display the output in a more ledgible manner.
 }
 
