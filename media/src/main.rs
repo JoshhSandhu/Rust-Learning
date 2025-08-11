1062f51f1adf8dfb520229ff2f8c05538981f0f0
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-08-11 21:40:07 +0530

    media completed

--- media/src/main.rs
+++ media/src/main.rs
@@ -1,113 +1,33 @@
-use std::{collections::hash_map::Values, num::FpCategory};
+//use std::{collections::hash_map::Values, num::FpCategory};
 
-#[derive(Debug)]
-enum media{
-    book{title: String, author: String},
-    movie{title: String, director: String},
-    Audiobook{title: String},
-    podcast(u32),
-    placeholder
-}
-//how would we work with a variant with raw data assigned to it and one with no data assigned
+mod content;
+use content::media::Media;
+use content::catalog::Calalog;
 
-impl media {
-    fn description(&self) -> String {
-
-        //pattern matching
-        //we dont know if self is an audiobook, movie or an book
-        // if let media::book{ title, author} = self {
-        //     format!("Book:{} {}", title, author)
-        // }
-        // else if let media::movie{title,director} = self{
-        //     format!("Movie:{} {}", title, director)
-        // }
-        // else if let media::Audiobook{title}=self{
-        //     format!("Audiobook: {}", title)
-        // }
-        // else{
-        //     String::from("media Description")
-        // }
-
-        match self{
-            media::book { title, author } =>{
-                format!("Book:{} {}", title, author)
-            }
-            media::movie { title, director } =>{
-                format!("Movie:{} {}", title, director)
-            }
-            media::Audiobook { title } =>{
-                format!("Audiobook: {}", title)
-            }
-            media::podcast(ep_num) => {
-                format!("Podcast:{}", ep_num)
-            }
-            media::placeholder =>{
-                format!("placeholder")
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Calalog{
-    item: Vec<media>
-}
-
-#[derive(Debug)]
-struct Account {
-    balance: i32
-}
-
-impl Calalog {
-    fn new() -> Self{
-        Calalog{ item: vec![]}
-    }
-
-    fn add(&mut self, media:media){
-        self.item.push(media);
-    }
-
-    fn get_by_index(&self, index: usize) -> Option<&media>{
-        if self.item.len() > index{
-            Some((&self.item[index]))
-        }
-        else{
-            None
-        }
-        
-    }
-}
-
-//this enum that we made is use to show the working of the default option enum
-// enum  mighHaveaValue<'a > {
-//     thereIsAvalue(&'a media),
-//     novalueAvalaible
-// }
-
-fn print_media(media:media){
-    println!("{:#?}", media)
+fn print_media(Media:Media){
+    println!("{:#?}", Media)
 }
 
 fn main() {
     println!("Enums in Rust");
     
-    let audiobook = media::Audiobook { 
+    let audiobook = Media::Audiobook { 
         title: String::from("Syntax") 
     };
     
-    let goodmovie = media::movie { 
+    let goodmovie = Media::movie { 
         title: String::from("interstller"), 
         director: String::from("Christopher Nolan") 
     };
 
-    let niceBook = media::book { 
+    let niceBook = Media::book { 
         title: String::from("harry potter"), 
         author: String::from("JK rowling") 
     };
 
-    let podcats = media::podcast((10));
+    let podcats = Media::podcast((10));
 
-    let Placeholder = media::placeholder;
+    let Placeholder = Media::placeholder;
 
     let mut calalog = Calalog::new();
 
@@ -118,7 +38,7 @@ fn main() {
     calalog.add(Placeholder);
     
     let item = calalog.get_by_index(40);
-    let holder = media::placeholder;
+    let holder = Media::placeholder;
     
     println!("{:#?}", item.unwrap_or(&holder));
 
@@ -156,7 +76,7 @@ fn main() {
     // println!("{}",goodmovie.description());
     // println!("{}",niceBook.description());
 
-    // print_media(audiobook);
-    // print_media(goodmovie);
-    // print_media(niceBook);
+    // print_Media(audiobook);
+    // print_Media(goodmovie);
+    // print_Media(niceBook);
 }
--- target/debug/deps/media-5701a8118ee2d19d.d
+++ target/debug/deps/media-5701a8118ee2d19d.d
@@ -1,5 +1,8 @@
-D:\mislanious\vs code\Rust_learning\Rust-Learning\target\debug\deps\media-5701a8118ee2d19d.d: media\src\main.rs
+D:\mislanious\vs code\Rust_learning\Rust-Learning\target\debug\deps\media-5701a8118ee2d19d.d: media\src\main.rs media\src\content\mod.rs media\src\content\media.rs media\src\content\catalog.rs
 
-D:\mislanious\vs code\Rust_learning\Rust-Learning\target\debug\deps\libmedia-5701a8118ee2d19d.rmeta: media\src\main.rs
+D:\mislanious\vs code\Rust_learning\Rust-Learning\target\debug\deps\libmedia-5701a8118ee2d19d.rmeta: media\src\main.rs media\src\content\mod.rs media\src\content\media.rs media\src\content\catalog.rs
 
 media\src\main.rs:
+media\src\content\mod.rs:
+media\src\content\media.rs:
+media\src\content\catalog.rs:
