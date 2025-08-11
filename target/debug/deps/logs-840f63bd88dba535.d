b8d5c2ba8fede1e7659da2fa046a6acb0c221f98
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-08-11 21:46:31 +0530

    logs project begins

---
+++ target/debug/deps/logs-840f63bd88dba535.d
@@ -0,0 +1,5 @@
+D:\mislanious\vs code\Rust_learning\Rust-Learning\target\debug\deps\logs-840f63bd88dba535.d: logs\src\main.rs
+
+D:\mislanious\vs code\Rust_learning\Rust-Learning\target\debug\deps\liblogs-840f63bd88dba535.rmeta: logs\src\main.rs
+
+logs\src\main.rs:
