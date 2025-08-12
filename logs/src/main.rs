49f70f065ac603b21562ea8891d2aef11e5e019c
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-08-12 22:13:26 +0530

    empty tuples and erros

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -2,10 +2,33 @@ use std::fs;
 use std::io::Error;
 fn main() {
 
-    let text = fs::read_to_string("logs.txt");
-    println!("{:#?}", text);
+    // let text = fs::read_to_string("logs.txt");
+    // println!("{:#?}", text);
+
+    match devide(5.0, 0.0){
+        Ok(result_of_division) =>{
+            println!("{}", result_of_division);
+        }
+        Err(what_went_wrong) =>{
+            println!("{}", what_went_wrong)
+        }
+    }
+
+    match validate_email(String::from("josh.sandhu@email.com")){
+        Ok(..) => println!("email is valid"),
+        Err(er) =>{
+            println!("{}", er)
+        }
+    }
 }
 
+fn validate_email(email : String) -> Result<(), Error>{
+    if email.contains("@"){
+        Ok(())
+    } else {
+        Err(Error::other("emails should contain @"))
+    } 
+}
 
 fn devide(a: f64, b: f64) -> Result<f64, Error>{
     if b == 0.0{
