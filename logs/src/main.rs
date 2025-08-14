06e48cdf4bc8b84dffce324b096cba680249b74d
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-08-14 19:43:46 +0530

    extracting the error logs form a file

--- logs/src/main.rs
+++ logs/src/main.rs
@@ -1,20 +1,25 @@
-use std::fs;
+use std::{fs, result};
 
-fn string_test(
-        a:String,
-        b:&String,
-        c:&str
-    ){
+fn extract_errors(text: &str) -> Vec<&str>{
+    let split_text = text.split("\n");
 
+    let mut results = vec![];
+
+    for line in split_text{
+        if line.starts_with("ERROR"){
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 fn main() {
 
-    string_test(String::from("red"), &String::from("red"), "red");
-
     match fs::read_to_string("logs.txt") {
         Ok(text_that_was_read) =>{
-            println!("{}", text_that_was_read.len());
+            let error_logs = extract_errors(text_that_was_read.as_str());
+            println!("{:#?}", error_logs);
         }
         Err(why_this_failed) =>{
             println!("failed to read file: {}", why_this_failed);
