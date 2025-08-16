d25900b2f6cb0237fa08ab847ab04df334ce6097
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-08-16 22:04:49 +0530

    using the collect() fn to collect the values

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -12,17 +12,29 @@ fn print_elements(elements : &[String]){ //this is slice of a vector
 }
 
 //shortens the strings in each vector to 1 char
-fn shortened_strings(short_elements: &mut Vec<String>){
+fn shortened_strings(short_elements: &mut [String]){
     short_elements.iter_mut().for_each(|el | el.truncate(1));
     //print_elements(&short_elements);
+}
+
+//return a new vector with each element capitalized
+fn to_uppercase(elements: &[String]) -> Vec<String>{
+    elements.iter()
+    .map(|el| el.to_uppercase())
+    .collect()
 }
 
 fn main() {
     let mut colors = vec![
         String::from("red"),
-        String::from("red"),
-        String::from("red")
+        String::from("blue"),
+        String::from("green")
     ];
-    shortened_strings(&mut colors);
+
+    let uppercased = to_uppercase(&colors);
+    println!("{:#?}", uppercased);
+    shortened_strings(&mut colors[1..3]);
     print_elements(&colors);
+
+
 }
