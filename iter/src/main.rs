0b4db18586f4e35bb463c8e79d91ec526c4b2707
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-08-16 21:09:09 +0530

    basics of an iterator

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let colors = vec![
+        String::from("red"),
+        String::from("red"),
+        String::from("red")
+    ];
+
+    let mut colors_iter = colors.iter();
+
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
+    println!("{:#?}", colors_iter.next());
 }
