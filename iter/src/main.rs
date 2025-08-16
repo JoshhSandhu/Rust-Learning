15b8b36ef67df442531defc24fd7f1ca19e726ce
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-08-16 23:43:46 +0530

    using into iter to change the owner of values

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -24,6 +24,12 @@ fn to_uppercase(elements: &[String]) -> Vec<String>{
     elements.iter()
     .map(|el| el.to_uppercase())
     .collect::<Vec<_>>() //type inference the compiler figures out the value that goes here
+    //this is called a trubofish
+}
+
+//moves elemts from one vector to another
+fn move_elements(Vec_A : Vec<String>, vec_B: &mut Vec<String>){ 
+    Vec_A.into_iter().for_each(|el| vec_B.push(el));
 }
 
 fn main() {
@@ -37,6 +43,10 @@ fn main() {
     println!("{:#?}", uppercased);
     shortened_strings(&mut colors[1..3]);
     print_elements(&colors);
+    
+    
+    let mut moved = vec![];
 
-
+    move_elements(colors, &mut moved);
+    println!("{:#?}", moved);
 }
